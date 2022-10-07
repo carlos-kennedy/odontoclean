113e8dbcafccbe70c727f61703ebc9c95c31af55
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-10-07 16:29:05 -0300

    Update

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const navBtn = document.querySelector("button, .hamburguer");
+const navUl = document.querySelector("ul, .active");
+const body = document.querySelector("body");
 const btnShowMenu = () => {
+  navUl.classList.toggle("active");
+  body.style.overflow = "hidden";
   navBtn.classList.toggle("is-active");
 };
 
