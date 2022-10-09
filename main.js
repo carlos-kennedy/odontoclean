9e0a64d9fd8599e646ae5f9e31c85d009960ffdf
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-10-08 22:51:35 -0300

    Update

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
-const navBtn = document.querySelector("button, .hamburguer");
+const html = document.querySelector("html");
 const navUl = document.querySelector("ul, .active");
 const logo = document.querySelector("svg, .active");
+const navBtn = document.querySelector("button, .hamburguer");
+const showLi = document.querySelector(".show-more-list");
+
+const btnShowList = document.querySelector(".btn-show-more");
+
 const btnShowMenu = () => {
   navUl.classList.toggle("active");
   navBtn.classList.toggle("is-active");
   logo.classList.toggle("active");
+  if (html.style.overflow == "hidden") {
+    html.style.overflow = "auto";
+  } else {
+    html.style.overflow = "hidden";
+  }
+};
+
+const showItems = () => {
+  showLi.classList.toggle("active");
 };
 
 navBtn.addEventListener("click", btnShowMenu);
+btnShowList.addEventListener("click", showItems);
