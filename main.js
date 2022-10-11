5272c32b1e2d39ec5e9eb1c6961f860980167e95
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-10-11 20:31:40 -0300

    Update mobile complete.

--- main.js
+++ main.js
@@ -3,8 +3,12 @@ const navUl = document.querySelector("ul, .active");
 const logo = document.querySelector("svg, .active");
 const navBtn = document.querySelector("button, .hamburguer");
 const showLi = document.querySelector(".show-more-list");
-
+const doubtList = document.querySelector(".doubt-list");
+const doubtItems = doubtList.getElementsByTagName("li");
+const divShowMore = document.querySelector(".show-more");
 const btnShowList = document.querySelector(".btn-show-more");
+
+// const btnShowTextsInDoubt = document.querySelector(".btn-show-text");
 
 const btnShowMenu = () => {
   navUl.classList.toggle("active");
@@ -19,7 +23,31 @@ const btnShowMenu = () => {
 
 const showItems = () => {
   showLi.classList.toggle("active");
+
+  if (divShowMore.style.position == "initial") {
+    divShowMore.style.position = "relative";
+  } else {
+    divShowMore.style.position = "initial";
+  }
+
+  if (btnShowList.innerText == "Mostrar mais") {
+    btnShowList.innerHTML = `<i class="fa-solid fa-angle-up"></i>
+                         Mostrar menos`;
+  } else {
+    btnShowList.innerHTML = `<i class="fa-solid fa-angle-down"></i>
+                         Mostrar mais`;
+  }
 };
+
+// Show items
+for (var i = 0; i < doubtItems.length; i++) {
+  doubtItems[i].addEventListener("click", function () {
+    document.querySelector(".list-active")
+      ? document.querySelector(".list-active").classList.remove("list-active")
+      : "";
+    this.classList.add("list-active");
+  });
+}
 
 navBtn.addEventListener("click", btnShowMenu);
 btnShowList.addEventListener("click", showItems);
---
+++ slider.js
@@ -0,0 +1,10 @@
+new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  spaceBetween: 15,
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: false,
+  },
+  // freeMode: true,
+  loop: false,
+});
